4359a02cf6ffe906682dd6120cfe7090aa0ea281
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-11-02 13:28:58 -0700

    Trying to create a ping command

---
+++ index.js
@@ -0,0 +1,63 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ("data" in command && "execute" in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log("[WARNING] The command at ${filePath} is missing a required 'data' or 'execute' property.");
+	}
+}
+
+console.log(client.commands);
+
+
+require("dotenv").config();
+
+
+// When the client is ready, run this code (only once)
+// We use "c" for the event parameter to keep it separate from the already defined "client"
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	console.log("Received interaction");
+
+
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+	}
+});
+
+
+// Log in to Discord with your client"s token
+const token = process.env.DISCORD_TOKEN;
+client.login(token);
