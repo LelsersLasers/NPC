6cb96cff08dfffdf2458b1c03648806801fb7555
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-11-02 13:46:09 -0700

    Sucessfully getting ping to work

--- index.js
+++ index.js
@@ -16,15 +16,10 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith("
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ("data" in command && "execute" in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log("[WARNING] The command at ${filePath} is missing a required 'data' or 'execute' property.");
-	}
+	client.commands.set(command.data.name, command);
 }
 
-console.log(client.commands);
+console.log("COMMANDS:", client.commands);
 
 
 require("dotenv").config();
