7d88d1ba9b911a6030cec6aa6c369bebd48278a6
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-11-07 12:51:48 -0800

    Imporving insult.js (adding multiple engines and finishing formula and evil engines)

--- commands/insult.js
+++ commands/insult.js
@@ -1,59 +1,123 @@
 const { SlashCommandBuilder } = require("discord.js");
 const fs = require("fs");
+const axios = require('axios');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("insult")
-    .setDescription("Generates an insult or insults someone (format: /insult [user])")
+    .setDescription(
+      "Generates an insult or insults someone (format: /insult [engine] [user])"
+    )
+    .addStringOption((option) =>
+      option
+        .setName("engine")
+        .setDescription("The engine to use for the insult")
+        .addChoices(
+          { name: "formula", value: "formula" }, // default
+          { name: "shakespearean", value: "shakespearean" },
+          { name: "evil", value: "evil" }
+        )
+    )
     .addUserOption((option) => option.setName("user").setDescription("User to insult")),
   async execute(interaction) {
     console.log("\n'/insult' command executed");
+    await interaction.deferReply();
 
     function dataToList(data) {
       return data
         .split("\n")
-        .map((word) => word.replace(/(\r\n|\n|\r)/gm, "").trim())
+        .map((word) =>
+          word
+            .replace(/(\r\n|\n|\r)/gm, "")
+            .trim()
+            .toLowerCase()
+        )
         .filter((word) => word != "");
     }
     function randomWord(lst) {
       return lst[Math.floor(Math.random() * lst.length)];
     }
+    function readShakespeareanInsults(path) {
+      const data = fs.readFileSync(path, "utf8");
+      const lines = data.split("\n");
+      let columns = { first: [], second: [], third: [] };
+      for (const line of lines) {
+        const words = line.split("s").map((word) =>
+          word
+            .replace(/(\r\n|\n|\r)/gm, "")
+            .trim()
+            .toLowerCase()
+        );
+        columns.first.push(words[0]);
+        columns.second.push(words[1]);
+        columns.third.push(words[2]);
+      }
+      return columns;
+    }
 
+    const engine = interaction.options.getString("engine") ?? "formula";
     const user = interaction.options.getUser("user");
+    console.log({ engine, user });
+    let insult = "";
+    let userInsult = "";
 
-    await interaction.deferReply();
     try {
-      const colorsData = fs.readFileSync("./words/colors.txt", "utf8");
-      const animalsData = fs.readFileSync("./words/animals.txt", "utf8");
-      const nounsData = fs.readFileSync("./words/nouns.txt", "utf8");
-      const adjectivesData = fs.readFileSync("./words/adjectives.txt", "utf8");
+      switch (engine) {
+        case "formula":
+          const adverbsData = fs.readFileSync("./words/adverbs.txt", "utf8");
+          const gerundsData = fs.readFileSync("./words/gerunds.txt", "utf8");
+          const colorsData = fs.readFileSync("./words/colors.txt", "utf8");
+          const adjectivesData = fs.readFileSync("./words/adjectives.txt", "utf8");
+          const animalsData = fs.readFileSync("./words/animals.txt", "utf8");
 
-      const colors = dataToList(colorsData);
-      const animals = dataToList(animalsData);
-      const nouns = dataToList(nounsData).filter(
-        (noun) => !animals.includes(noun)
-      );
-      const adjectives = dataToList(adjectivesData).filter(
-        (adjective) => !colors.includes(adjective)
-      );
+          const adverbs = dataToList(adverbsData);
+          const gerunds = dataToList(gerundsData);
+          const colors = dataToList(colorsData);
+          const adjectives = dataToList(adjectivesData).filter(
+            (adjective) => !colors.includes(adjective)
+          );
+          const animals = dataToList(animalsData);
 
-      const color = randomWord(colors);
-      const animal = randomWord(animals);
-      const noun = randomWord(nouns);
-      const adjective = randomWord(adjectives);
-      console.log({ color, animal, adjective, noun });
+          const adverb = randomWord(adverbs);
+          const gerund = randomWord(gerunds);
+          const adjective = randomWord(adjectives);
+          const color = randomWord(colors);
+          const animal = randomWord(animals);
+          console.log({ adverb, gerund, adjective, color, animal });
 
-      const insult = adjective + " " + color + " " + animal + " " + noun;
+          insult = `${adverb} ${gerund} ${adjective} ${color} ${animal}`;
+          userInsult = `you ${insult}!`;
+          break;
+        case "shakespearean":
+          const columns = readShakespeareanInsults("./words/shakespeare.txt");
 
+          const first = randomWord(columns.first);
+          const second = randomWord(columns.second);
+          const third = randomWord(columns.third);
+          console.log({ first, second, third });
+
+          insult = `${first} ${second} ${third}`;
+          userInsult = `thou ${insult}!`;
+          break;
+        case "evil":
+          const data = await axios.get(
+            "https://evilinsult.com/generate_insult.php?lang=en&type=json"
+          ).then((response) => response.data);
+          console.log(data);
+
+          insult = "\n" + data.insult;
+          userInsult = data.insult;
+          break;
+      }
       if (user) {
-        await interaction.editReply(user.toString() + ", you " + insult + "!");
+        await interaction.editReply(user.toString() + ", " + userInsult);
       } else {
         await interaction.editReply("Insult generated: " + insult);
       }
     } catch (error) {
-      console.error("\nError reading files for insult command:\n");
+      console.error("\nError during 'insult' command:\n");
       console.error(error);
-      await interaction.editReply("Error reading files for insult command");
+      await interaction.editReply("Error during 'insult' command");
     }
   },
 };
