322a98aa618a975e9e5cabe746597a6440f664f7
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2023-08-20 20:49:34 -0700

    Adding League of Graphs engine as option to U.GG

--- commands/league.js
+++ commands/league.js
@@ -4,7 +4,16 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("league")
     .setDescription(
-      "League builds or counters (format: /league <champion> <type> <role> <opponent>)"
+      "League builds or counters (format: /league <engine> <champion> <type> <role> <opponent>)"
+    )
+    .addStringOption((option) =>
+      option
+        .setName("engine")
+        .setDescription("Which website to use for builds (default: u.gg)")
+        .addChoices(
+            { name: "u.gg", value: "u.gg" },
+            { name: "leagueofgraphs.com", value: "leagueofgraphs.com" }
+        )
     )
     .addStringOption((option) =>
       option
@@ -23,7 +32,7 @@ module.exports = {
     .addStringOption((option) =>
       option
         .setName("role")
-        .setDescription("(only relevant if type=build and champion is specified) (default: most popular for champion)")
+        .setDescription("(only relevant if type=build and champion is specified) (default: most popular or all)")
         .addChoices(
             { name: "top", value: "top" },
             { name: "jungle", value: "jungle" },
@@ -44,34 +53,54 @@ module.exports = {
         return str.replace(/[^a-zA-Z]/g, "");
     }
 
+    const engine = interaction.options.getString("engine") ?? "u.gg";
     const champion = removeAllNonLetters(interaction.options.getString("champion") ?? "");
     const type = interaction.options.getString("type") ?? "build";
     const role = interaction.options.getString("role") ?? "";
     const opponent = removeAllNonLetters(interaction.options.getString("opponent") ?? "");
 
-    const noChampionLink = `<https://u.gg/lol/tier-list>`;
     if (champion == "") {
-        await interaction.reply(`League builds:\n${noChampionLink}`);
+        const noChampionLinkUgg = `<https://u.gg/lol/tier-list>`;
+        const noChampionLinkLog = `<https://www.leagueofgraphs.com/champions/builds/by-winrate>`;
+        if (engine == "u.gg") {
+            await interaction.reply(`League builds from U.GG:\n${noChampionLinkUgg}`);
+        } else {
+            await interaction.reply(`League builds from League Of Graphs:\n${noChampionLinkLog}`);
+        }
         return;
     }
 
+    let linkString = "";
+    if (engine == "u.gg") {
+        const linkStringBase = `<https://u.gg/lol/champions/${champion}/${type}>`;
+        linkString = linkStringBase;
 
-    const linkStringBase = `<https://u.gg/lol/champions/${champion}/${type}>`;
-    let linkString = linkStringBase;
-
-    if (type == "build") {
-        if (role != "") {
-            linkString += `/${role}`;
+        if (type == "build") {
+            if (role != "") {
+                linkString += `/${role}`;
+            }
+            if (opponent != "") {
+                linkString += `?opp=${opponent}`;
+            }
         }
-        if (opponent != "") {
-            linkString += `?opp=${opponent}`;
+    } else {
+        const linkStringBase = `<https://www.leagueofgraphs.com/champions/${type}s/${champion}>`;
+        linkString = linkStringBase;
+
+        if (type == "build") {
+            if (role != "") {
+                linkString += `/${role}`;
+            }
+            if (opponent != "") {
+                linkString += `/${opponent}`;
+            }
         }
     }
 
 
-    let output = "";
+    let output = engine == "u.gg" ? "U.GG " : "League Of Graphs ";
     if (type == "build") {
-        output += `Builds for `;
+        output += `builds for `;
         if (role != "") {
             output += `${role} `;
         }
@@ -80,7 +109,7 @@ module.exports = {
             output += ` vs ${opponent}`;
         }
     } else {
-        output += `Counters for ${champion}`;
+        output += `counters for ${champion}`;
     }
     output += `:\n${linkString}`;
 
